10e1085aa832f89c78b95f14fd084273a154a5fc
Danilo Ricardo Dias Pena <danilord.pena@gmail.com>
2017-06-23 15:45:44 -0300

    Initial Commit

---
+++ GraphicView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GraphicView
+//
+//  Created by Cast on 6/23/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GraphicView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GraphicView
+//
+//  Created by Cast on 6/23/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
